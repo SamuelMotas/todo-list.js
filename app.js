dae219bc4431560497ddaa38e55951d57e9c99cd
Samuel Motas <smlmotas@gmail.com>
2025-10-18 16:14:02 -0400

    Ajout de la To-Do List Dark Souls avec modification et suppression

--- app.js
+++ app.js
@@ -9,6 +9,44 @@ function addTask() {
         return;
     }
 
-    document.createElement("li")
+    let li = document.createElement("li");
+    li.innerHTML = taskText;
 
+    let editButton = document.createElement("button")
+    editButton.innerHTML = '<ion-icon name="pencil-outline"></ion-icon>'
+
+    editButton.onclick = function () {
+        editTask(li)
+    }
+
+    let deleteButton = document.createElement("button")
+    deleteButton.innerHTML = '<ion-icon name="trash-outline"></ion-icon>'
+
+    deleteButton.onclick = function () {
+        deleteTask(li)
+    }
+
+    li.appendChild(editButton);
+    li.appendChild(deleteButton);
+
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+}
+
+function editTask(task) {
+    let taskTextElement = task.firstChild;
+    let taskText = taskTextElement.textContent;
+
+    let newTaskText = prompt("Modifier la tâche:", taskText);
+
+    if (newTaskText === null || newTaskText === "") {
+        return;// Ne rien faire si l'utilisateur a cliqué sur Annuler ou n'a pas entré de nouveau texte
+    }
+
+    taskTextElement.textContent = newTaskText;
+}
+
+function deleteTask(task){
+    taskList.removeChild(task);
 }
